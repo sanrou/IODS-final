17cfc058343d813994e707e156e84c0b4dcfa008
sanrou <rouhinen@gmail.com>
2017-12-15 18:05:30 +0200

    Hopefully finalized the final assignment.

--- createLearning2014.R
+++ createLearning2014.R
@@ -1,5 +1,5 @@
 # Santeri Rouhinen, 2017.12.12. santeri.rouhinen@helsinki.fi
-# Load learning dataset and wrangle it.
+# Load learning dataset and wrangle it. Save as .csv file.
 # Link with information about the dataset:
 # http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS2-meta.txt
 
@@ -17,74 +17,78 @@ lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt
 ### Collapse questions into guestion classes. 
 
 ## Collapse most of the variables like in the JYTOPKYS2-meta.txt file: 
+# Take a mean of the different questions so that scale is kept at 1-5.
 #   *Lar       Aa + Ac + Ad    *Learning as Reproducing
-lar <- lrn14$Aa + lrn14$Ab + lrn14$Ad
+lar <- rowMeans(lrn14[c('Aa', 'Ab', 'Ad')])
 learning2014 <- data.frame(lar)
 
 # *Lat       Ab + Ae + Af      *Learning as Transforming
-learning2014$lat <- lrn14$Ab + lrn14$Ae + lrn14$Af
+learning2014$lat <- rowMeans(lrn14[c('Ab', 'Ae', 'Af')])
 
 #   *d_sm      D03 + D11 + D19 + D27     *Seeking Meaning, deep
-learning2014$d_sm <- lrn14$D03 + lrn14$D11 + lrn14$D19 + lrn14$D27
+learning2014$d_sm <- rowMeans(lrn14[c('D03', 'D11', 'D19', 'D27')])
 
 # *d_ri      D07 + D14 + D22 + D30       *Relating Ideas, deep
-learning2014$d_ri <- lrn14$D07 + lrn14$D14 + lrn14$D22 + lrn14$D30
+learning2014$d_ri <- rowMeans(lrn14[c('D07', 'D14', 'D22', 'D30')])
 
 # *d_ue      D06 + D15 + D23 + D31       *Use of Evidence, deep
-learning2014$d_ue <- lrn14$D06 + lrn14$D15 + lrn14$D23 + lrn14$D31
+learning2014$d_ue <- rowMeans(lrn14[c('D06', 'D15', 'D23', 'D31')])
 
 # *su_lp     SU02 + SU10 + SU18 + SU26   *Lack of Purpose, surface
-learning2014$su_lp <- lrn14$SU02 + lrn14$SU10 + lrn14$SU18 + lrn14$SU26
+learning2014$su_lp <- rowMeans(lrn14[c('SU02', 'SU10', 'SU18', 'SU26')])
 
 # *su_um     SU05 + SU13 + SU21 + SU29   *Unrelated Memorising, surface
-learning2014$su_um <- lrn14$SU05 + lrn14$SU13 + lrn14$SU21 + lrn14$SU29
+learning2014$su_um <- rowMeans(lrn14[c('SU05', 'SU13', 'SU21', 'SU29')])
 
 # *su_sb     SU08 + SU16 + SU24 + SU32   *Syllabus-boundness, surface
-learning2014$su_sb <- lrn14$SU08 + lrn14$SU16 + lrn14$SU24 + lrn14$SU32
+learning2014$su_sb <- rowMeans(lrn14[c('SU08', 'SU16', 'SU24', 'SU32')])
 
 # *st_os     ST01 + ST09 + ST17 + ST25   *Organized Studying, strategic
-learning2014$st_os <- lrn14$ST01 + lrn14$ST09 + lrn14$ST17 + lrn14$ST25
+learning2014$st_os <- rowMeans(lrn14[c('ST01', 'ST09', 'ST17', 'ST25')])
 
 # *st_tm     ST04 + ST12 + ST20 + ST28   *Time Management, strategic
-learning2014$st_tm <- lrn14$ST04 + lrn14$ST12 + lrn14$ST20 + lrn14$ST28
+learning2014$st_tm <- rowMeans(lrn14[c('ST04', 'ST12', 'ST20', 'ST28')])
 
 #   *Su_Ti     Ca + Cd + Ce + Ch   *Supporting understanding (related to a deep approach)
-learning2014$su_ti <- lrn14$Ca + lrn14$Cd + lrn14$Ce + lrn14$Ch
+learning2014$su_ti <- rowMeans(lrn14[c('Ca', 'Cd', 'Ce', 'Ch')])
 
 # *D_Su      Cb + Cc + Cf + Cg     *Transmitting information (related to a surface approach)
-learning2014$d_su <- lrn14$Cb + lrn14$Cc + lrn14$Cf + lrn14$Cg
+learning2014$d_su <- rowMeans(lrn14[c('Cb', 'Cc', 'Cf', 'Cg')])
 
 #   *Obs!! Df and Dh must be reversed first:
 lrn14$Df <- 6 - lrn14$Df
 lrn14$Dh <- 6 - lrn14$Dh
 
 #   *Stat_Conf           Da + Df  *Confidence in doing statistics
-learning2014$stat_conf <- lrn14$Da + lrn14$Df
+learning2014$stat_conf <- rowMeans(lrn14[c('Dh', 'Df')])
 
 # *Value     Db + Dj              *Value of statistics
-learning2014$value <- lrn14$Db + lrn14$Dj
+learning2014$stat_value <- rowMeans(lrn14[c('Db', 'Dj')])
 
 # *Interest  Dc + De             *Interest in statistics
-learning2014$interest <- lrn14$Dc + lrn14$De
+learning2014$interest <- rowMeans(lrn14[c('Dc', 'De')])
 
 # *Math_Conf           Dd + Di   *Confidence in doing math
-learning2014$math_conf <- lrn14$Dd + lrn14$Di
+learning2014$math_conf <- rowMeans(lrn14[c('Dd', 'Di')])
 
 # *Affect    Dg + Dh             *Affect toward statistics
-learning2014$affect <- lrn14$Dg + lrn14$Dh
-
-
-## Scale the variables to a mean of 0 and SD of 1.
-learning2014 <- scale(learning2014)
+learning2014$affect <- rowMeans(lrn14[c('Dg', 'Dh')])
 
 
 ## Take original values from lrn14
 take_columns <- c("gender","Age","Points")
 learning2014 <- cbind(learning2014, lrn14[take_columns])
 
+# Make gender boolean for easier visualizations. Rename to gender_m so one can remember what does 0 and 1 mean.
+learning2014$gender <- learning2014$gender == 'M'
+names(learning2014)[names(learning2014) == 'gender'] <- 'gender_m'
 
-## Rename column names so that Age and Points are not capitalized. 
+## Rename column names so that Age and Points are not capitalized.
 names(learning2014) <- sapply(names(learning2014), tolower)
+
+## Sort the variables alphabetically
+learning2014 <- learning2014[ , order(names(learning2014))]
+
 
 
 ## Save data. There should be only a few files so save to root.
--- tempFinal.R
+++ tempFinal.R
@@ -2,12 +2,53 @@
 
 # Initialize required libraries
 library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(corrplot)
+library(ROCR)
+
 
 # Read learning2014.csv
 learning2014 <- read.csv("learning2014.csv",row.names = 1)
 
 # Check how many did not take the exam. Answer is 17.
 sum(learning2014$points == 0)
+
+
+## Make two histograms to have a different amount of bins. This will reduce ugliness.
+# Index variables with originally 2 questions classes.
+index2 <- c(1, 8, 11, 15, 16)
+
+# Make histogram of the variables with originally 2 classes.
+learning2014[index2] %>% 
+  gather(key=var_name, value = value) %>% 
+  ggplot(aes(x=value)) +
+  geom_histogram(bins = 9) +
+  facet_wrap(~var_name, scales = "free_x")
+
+
+# Make histograms of the rest.
+learning2014[-index2] %>% 
+  gather(key=var_name, value = value) %>% 
+  ggplot(aes(x=value)) +
+  geom_histogram(bins = 15) +
+  facet_wrap(~var_name, scales = "free_x")
+
+
+# Make a correlation plot
+cor_matrix<-cor(learning2014) 
+
+# Create color ramp from dull dark blue to white to dull red.
+colorVector <- c("#4477AA", "#77AADD", "#FFFFFF", "#EE9988", "#BB4444")
+
+# Visualize correlation matrix
+corrplot(cor_matrix, method = "color", col = colorRampPalette(colorVector)(200),
+         type = "upper", order = "alphabet", number.cex = .8,
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col = "black", tl.srt = 30, # Text label color and rotation
+         # hide correlation coefficient on the principal diagonal
+         diag = FALSE)
+
 
 # Split the learning2014 to exam takers and non-takers.
 examY <- filter(learning2014, points > 0)
@@ -25,8 +66,8 @@ testSet  <- rbind(examY[-(1:yTrainN) ,], examN[-(1:nTrainN) ,])
 # First list the columns
 colnames(learning2014)
 
-# Hand pick promising variables
-subVariables <- c("su_lp", "st_os", "st_tm", "stat_conf", "value", "interest", "math_conf", "affect")
+# Hand pick promising variables, keep alphabetical order.
+subVariables <- c("affect", "interest", "math_conf", "st_os", "st_tm", "stat_conf", "stat_value", "su_lp") %>% sort()
 
 # Change points to be boolean of taking exam
 trainSet$takeExam <- trainSet$points > 0
@@ -40,8 +81,9 @@ handSet  <- trainSet[c(subVariables, 'takeExam')]
 
 
 ## I'll use a linear model. Logistic regression model (glm).
-fullModel <- glm(takeExam ~ ., data = trainSet, family = "binomial")
-handModel <- glm(takeExam ~ ., data = handSet,  family = "binomial")
+fullModel <- glm(takeExam ~ ., data = trainSet, family = binomial(link = "logit"))
+handModel <- glm(takeExam ~ ., data = handSet,  family = binomial(link = "logit"))
+
 
 
 # Predict the probability of taking exam
@@ -60,7 +102,7 @@ handSet  <- mutate(handSet,  prediction = probability > 0.5)
 table(takeExam = trainSet$takeExam, prediction = trainSet$prediction)
 table(takeExam = handSet$takeExam,  prediction = handSet$prediction)
 
-# Well that didn't go too well. The full set performed better, but poorly (2 true negatives and 1 false negative, 11 false positives). The hand picked model had 1 false negative and 13 false positives, and 0 true negatives.
+# Well that didn't go too well. The full set performed better, but poorly (only 2 true negatives, 2 false negative, 11 false positives). The hand picked model predicted that all of the students would have taken the exam!
 
 # Search for a threshold in prediction which will give 13 predictions. So just sort the probabilities and find the 13th and 14th values, take average. 
 fullProbabilitySorted <- sort(trainSet$probability)
@@ -69,7 +111,9 @@ thresholdFull <- mean(fullProbabilitySorted[c(13,14)])
 handProbabilitySorted <- sort(handSet$probability)
 thresholdHand <- mean(handProbabilitySorted[c(13,14)])
 
-# The new thresholds are 0.6919 (full) and 0.7941 (hand picked).
+print(paste('Full model threshold:', thresholdFull, '. Hand picked model threshold:', thresholdHand))
+
+# The new thresholds are 0.704 (full) and 0.788 (hand picked).
 
 # Stir the pot and do the predictions again.
 trainSet <- mutate(trainSet, prediction = probability > thresholdFull)
@@ -94,10 +138,10 @@ fitHand <- ifelse(fitHand > thresholdHand, 1, 0)
 classificationErrorFull <- mean(fitHand != testSet$takeExam)
 print(paste('Accuracy of the hand picked model', 1 - classificationErrorFull))
 
-# The accuracies seem pretty decent if one didn't know better. Guessing that all will take the exam would have a better accuracy (1 - nDidNotTakeExam/All = 1 - 4/46 = 0.91). Using ROC to compute the area under curve might be a better idea. It is a commonly used performance measurement for binary classifier performance.
+# The accuracies seem pretty decent if one didn't know better. Guessing that all will take the exam would have about the same accuracy (1 - nDidNotTakeExam/All = 1 - 4/46 = 0.91). Using ROC to compute the area under curve might be a better idea. It is a commonly used performance measurement for binary classifier performance.
 
-library(ROCR)
 
+## Validation of the model with ROC and AUC.
 # ROC full model
 p <- predict(fullModel, newdata=testSet, type="response")
 pr <- prediction(p, testSet$takeExam)
@@ -124,9 +168,9 @@ plot(prfH)
 aucH <- performance(prH, measure = "auc")
 aucH <- aucH@y.values[[1]]
 print(paste('AUC of the hand picked model', aucH))
-aucH
 
-# Bleugh. Both models' predictive power suck. The full model might be a bit better (AUC 0.55 vs 0.51) but they are very close. Thus my own prediction of the the models performing about the same holds true. Too bad that they perform rather as poorly so it's hard to say if there is actually any sense in the models.
+
+# Bleugh. Both models' predictive power suck. The full model might be a bit better (AUC 0.51 vs 0.48) but they are both very close close to 0.5 (which is chance level). Thus my own prediction of the the models performing about the same holds true. Too bad that they perform rather as poorly so it's hard to say if there is actually any sense in the models.
 
 # Check the summaries of the models out of curiosity.
 summary(fullModel)
@@ -136,3 +180,9 @@ summary(fullModel)
 summary(handModel)
 
 # Same thing with the hand picked variables. I find it a bit peculiar that confidence in statistics is not even close significant. I would have assumed the variables to correlate strongly. In the hand picked model the intercept becomes highly significant instead of merely marginally significant. 
+
+# Use ANOVA to see how the models perform against null model (the intercept).
+
+anova(fullModel, test = "Chisq")
+anova(handModel, test = "Chisq")
+
